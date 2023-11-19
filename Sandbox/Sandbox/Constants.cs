70ac4b70222651ef12b41657a8c537a41195a8d3
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-11-19 23:42:58 +0200

    Added Constants file

---
+++ Sandbox/Sandbox/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class Constants
+    {
+        public const string CONTENT_REGION = "ContentRegion";
+
+        public const string CURRENCY_DETAILS_PAGE = "CurrencyDetailsPage";
+        public const string MAIN_PAGE = "MainPage";
+
+        //Path images
+        public const string CLEAR_LIGHT = "/images/ic_clear.png";
+        public const string SEARCH_LIGHT = "/images/ic_search.png";
+
+
+        //Name of menu items
+        public const string MAIN_ITEM = "Main";
+        public const string DETAILS_ITEM = "Details";
+        public const string SETTINGS_ITEM = "Settings";
+
+    }
+}
