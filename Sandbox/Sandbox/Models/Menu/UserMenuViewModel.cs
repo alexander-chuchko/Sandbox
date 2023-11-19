4841b4f49396714070c26d3ca05343bf7f35162e
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-11-19 23:36:00 +0200

    Added UserMenuViewModel

---
+++ Sandbox/Sandbox/Models/Menu/UserMenuViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Mvvm;
+
+namespace Sandbox.Models.Menu
+{
+    public class UserMenuViewModel : BindableBase
+    {
+        private string? _textForeground;
+        public string? TextForeground
+        {
+            get { return _textForeground; }
+            set { SetProperty(ref _textForeground, value); }
+        }
+
+        private string? _name;
+        public string? Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private string? _textBackground;
+        public string? TextBackground
+        {
+            get { return _textBackground; }
+            set { SetProperty(ref _textBackground, value); }
+        }
+
+        private string? _imageSource;
+        public string? ImageSource
+        {
+            get { return _imageSource; }
+            set { SetProperty(ref _imageSource, value); }
+        }
+    }
+}
