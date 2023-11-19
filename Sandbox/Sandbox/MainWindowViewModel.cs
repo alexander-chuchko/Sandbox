2083908a242146d291e23dadb954ba8b420edf07
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-11-19 23:40:32 +0200

    Added MainWindowViewModel

---
+++ Sandbox/Sandbox/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualBasic;
+using Prism.Mvvm;
+using Prism.Regions;
+using Sandbox.Models.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class MainWindowViewModel : BindableBase, IRegionMemberLifetime
+    {
+        private readonly IRegionManager _regionManager;
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager; 
+        }
+
+        private IEnumerable<UserMenuViewModel>? _menuList;
+        public IEnumerable<UserMenuViewModel>? MenuList
+        {
+            get { return _menuList; }
+            set { SetProperty(ref _menuList, value); }
+        }
+
+        public bool KeepAlive => throw new NotImplementedException();
+
+        private void CreatingMenuItems()
+        {
+            var menu = new List<UserMenuViewModel>()
+            {
+              new UserMenuViewModel()
+              {
+                  Name = Constants.MAIN_ITEM,
+                  TextBackground = "#f8f5f5",
+                  TextForeground = "#ff4064",
+                  ImageSource = "/Images/home_light.png"
+              },
+              new UserMenuViewModel()
+              {
+                  Name = Constants.DETAILS_ITEM,
+                  TextBackground =  Color.Transparent.ToString(),
+                  TextForeground = "#596EFB",
+                  ImageSource = "/Images/info_light.png"
+              },
+              new UserMenuViewModel()
+              {
+                  Name = Constants.SETTINGS_ITEM,
+                  TextBackground =  Color.Transparent.ToString(),
+                  TextForeground = "#596EFB",
+                  ImageSource = "/Images/settings_light.png"
+              },
+            };
+
+            MenuList = menu;
+        }
+    }
+}
