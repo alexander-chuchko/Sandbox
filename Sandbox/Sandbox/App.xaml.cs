97d6eb8723ee137d0963c6255d1966a6035dbec2
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-11-19 21:18:45 +0200

    Added Prism library

---
+++ Sandbox/Sandbox/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+
+namespace Sandbox
+{
+    public partial class App : PrismApplication
+    {
+        #region  ---   Overrides   ---
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //Registering services
+
+        }
+
+        #endregion
+    }
+}
